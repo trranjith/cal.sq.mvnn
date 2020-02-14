cda42adfe1b4631dba42b1fa36b7a6ce4920cc3a
Ramesh, Ranjith Kumar <ranjith.kumar.ramesh@accenture.com>
2020-02-14 11:33:09 +0530

    Modified Calc.java

--- src/main/java/calc/calc/sq/mvn/Calc.java
+++ src/main/java/calc/calc/sq/mvn/Calc.java
@@ -2,17 +2,16 @@ package calc.calc.sq.mvn;
 /*
  * This code is for simple arithmetic calculation
  */
-public class Calc extends java.lang.Thread{
+public class Calc {
     public double doAdd(double a, double b){
-    	double sum = Double.longBitsToDouble(123);
+    	double sum = 0;
     	sum = a + b;
         return sum;
     }
     public double doSub(double num1, double num2){
-    	int value = new Integer(123);
-        double SUB = 0;
-        SUB = num1 - num2;
-    	return SUB;
+        double sub = 0;
+        sub = num1 - num2;
+    	return sub;
     }
     public double doMul(double num1, double num2){
         return num1 * num2;
